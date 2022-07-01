25d64a770b8e002010e3352dc8377cb3c4fb2bd4
Prayag Sangode <prayag.rhce@gmail.com>
2022-07-01 18:48:04 +0530

    update vars tf code

--- provider.tf
+++ provider.tf
@@ -1,6 +1,4 @@
   provider "aws" {
   region = "${var.region}"
-  shared_config_files      = [""]
-  shared_credentials_files = ["/var/lib/jenkins/.aws/credentials"]
-  profile                  = ""
+  shared_credentials_file = "/var/lib/jenkins/.aws/credentials"
 }
