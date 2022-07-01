35c2774f793ca68f2a329bc1b48bc50a463c14eb
Prayag Sangode <prayag.rhce@gmail.com>
2022-07-01 17:19:59 +0530

    add iac code

---
+++ vars.tf
@@ -0,0 +1,16 @@
+variable "bucket_name" {
+    default = "s3-demo-19159"
+}
+variable "acl_value" {
+    default = "private"
+}
+variable "region" {
+    default = "us-east-1"
+}
+variable "aws_access_key" {
+    default = "<add-ur-key>"
+}
+variable "aws_secret_key" {
+    default = "<add-ur-key>"
+}
+
