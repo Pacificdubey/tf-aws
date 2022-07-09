2869c6de99e7e35b93f21234337d3e115e185e4c
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2022-07-09 23:52:32 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
-terraform {
-  backend "s3" {
+#terraform {
+  #backend "s3" {
     #bucket = "tf-s3-19159"
-    key    = "terraform.tstate"
-    region = "us-east-1"
+    #key    = "terraform.tstate"
+    #region = "us-east-1"
     #dynamodb_table = "tf-lock-table"
-  }
-}
+  #}
+#}
   
 provider "aws" {
   region = "${var.region}"
@@ -13,11 +13,11 @@ provider "aws" {
 }
 
 #provider "aws" {
-#  access_key = "${var.aws_access_key}"
-#  secret_key = "${var.aws_secret_key}"
-#  region = "${var.region}"
+  #access_key = "${var.aws_access_key}"
+  #secret_key = "${var.aws_secret_key}"
+  #region = "${var.region}"
 #}
 
 #provider "aws" {
-#  region  = "${var.region}"
+  #region  = "${var.region}"
 #}
