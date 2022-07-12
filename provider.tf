f9b44d9a4282d0d13f6b9c72661aab964153d0b5
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2022-07-13 00:45:58 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,3 @@
-<<<<<<< Updated upstream
-#terraform {
-  #backend "s3" {
-    #bucket = "tf-s3-19159"
-    #key    = "terraform.tstate"
-    #region = "us-east-1"
-    #dynamodb_table = "tf-lock-table"
-  #}
-#}
-=======
 terraform {
   backend "s3" {
     bucket = "tf-ts-s3-19159"
@@ -15,18 +5,7 @@ terraform {
     region = "us-east-1"
   }
 }
->>>>>>> Stashed changes
   
 provider "aws" {
   region = "${var.region}"
 }
-
-#provider "aws" {
-  #access_key = "${var.aws_access_key}"
-  #secret_key = "${var.aws_secret_key}"
-  #region = "${var.region}"
-#}
-
-#provider "aws" {
-  #region  = "${var.region}"
-#}
